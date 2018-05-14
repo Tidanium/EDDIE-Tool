c92921888fd436626f38d5cbb9c769a20b096510
Tidanium <ryan@ryanphillips.org>
2018-05-13 22:06:03 -0700

    Upload 2018-05-14 05:05:32Z

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # EDDIE-Tool
-Uptaking of the seemingly abandoned project, EDDIE-Tools, located at http://eddie-tool.psychofx.com/, refactored for Python 3.6.x and above.
+Uptaking of the seemingly abandoned project, EDDIE-Tools, located at http://eddie-tool.psychofx.com/, refactored for Python 3.6.0 and above.
---
+++ eddietool/version.py
@@ -0,0 +1,42 @@
+'''
+File             : version.py
+
+Start Date       : 20071209
+
+Description      : Version info
+
+$Id: version.py 953 2018-04-13 02:07:01Z phillips.ryan $
+'''
+__version__ = '$Revision: 953 $'
+
+__copyright__ = 'Copyright (c) Ryan Phillips 2018'
+
+__author__ = 'Chris Miles'
+__author_email__ = 'miles.chris@gmail.com'
+
+__maintainer__ = 'Ryan Phillips aka Tidanium'
+__maintainer_email__ = 'ryan@ryanphillips.org'
+
+__license__ = """
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+version = '2.0.0a1-dev'
